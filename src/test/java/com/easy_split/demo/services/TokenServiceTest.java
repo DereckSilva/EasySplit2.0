5ad9c9d8f1bc6f6e50c469b20c313ca3b225993b
Derek Vinicius Silva <viniciusdereck39@gmail.com>
2025-11-07 00:24:56 -0300

    criacao de testes parciais

---
+++ src/test/java/com/easy_split/demo/services/TokenServiceTest.java
@@ -0,0 +1,49 @@
+package com.easy_split.demo.services;
+
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.easy_split.demo.entities.User;
+import com.easy_split.demo.enums.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class TokenServiceTest {
+
+    @Test
+    @DisplayName("Esse método retorna um token para o usuário")
+    void generateToken() {
+        TokenService tokenService = mock(TokenService.class);
+        User user = new User(1, "teste@gmail.com", "12345", Role.USER);
+        String tokenCriado = retornaToken("token-criado");
+        when(tokenService.generateToken(user)).thenReturn(tokenCriado);
+
+        String token = tokenService.generateToken(user);
+        assertEquals(tokenCriado, token);
+    }
+
+    @Test
+    @DisplayName("Esse método verifica se o token informado para o usuário é válido")
+    void getSubjectFromToken() {
+        TokenService tokenService = mock(TokenService.class);
+        when(tokenService.getSubjectFromToken(retornaToken("ola-mundo"))).thenReturn("token-usuario-requisicao");
+        String tokenValid = tokenService.getSubjectFromToken(retornaToken("ola-mundo"));
+        assertEquals("token-usuario-requisicao", tokenValid);
+    }
+
+    @Test
+    @DisplayName("Esse método retorna uma exception de token expirado ou invalido")
+    void getSubjectFromTokenException() {
+        TokenService tokenService = mock(TokenService.class);
+        when(tokenService.getSubjectFromToken(retornaToken("token-expirado"))).thenThrow(new JWTCreationException("Token expirado ou inválido", new Exception()));
+        assertThrows(JWTCreationException.class, () -> tokenService.getSubjectFromToken(retornaToken("token-expirado")));
+    }
+
+    private String retornaToken (String token) {
+        byte[] tokenBytes = token.getBytes(StandardCharsets.UTF_8);
+        return UUID.nameUUIDFromBytes(tokenBytes).toString();
+    }
+}
